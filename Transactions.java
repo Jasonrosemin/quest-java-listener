b2ddf19c65b380c270b6966465eeea406985cdad
rosemin <jasonliverpool@hotmail.fr>
2019-12-08 22:49:01 +0100

    quest done

--- Transactions.java
+++ Transactions.java
@@ -3,12 +3,13 @@ import java.util.TimerTask;
 
 public class Transactions {
 
-    public static void withdraw(Bank account, int value) {
+    public static void withdraw(Bank account, int value, TransactionListener transaction ) {
         int time = (int) (Math.random() * ((1000 - 500) + 1)) + 500;
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
                 account.setTotal(account.getTotal() - value);
+                transaction.onComplete(account);
             }
         }, time);
     }
